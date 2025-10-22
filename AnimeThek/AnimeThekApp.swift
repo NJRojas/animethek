11e0fdd41c5eb44c4012259e60cd37de8854ac1e
Neyla Johanna Rojas Pena <Neyla.Rojas@de.airbus.com>
2025-10-22 19:20:03 +0200

    Initial Commit

---
+++ AnimeThek/AnimeThekApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeThekApp.swift
+//  AnimeThek
+//
+//  Created by NJ Rojas on 22.10.25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeThekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
